20b942642f270c210fa7ad8ca88178215408dff5
MiguelCarmuega <miguelcarmu98@gmail.com>
2025-11-17 00:45:38 +0100

    update

--- primerProyectoFundamentos/src/Main.java
+++ primerProyectoFundamentos/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ejecución de métodos");
+        saludar("Borja");
+        saludar("Miguel");
+        saludar("Luis");
+        despedir();
 
-    
+        Operaciones calculadora = new Operaciones();
+        calculadora.sumar(3,4);
+        calculadora.sumar(5,6);
+        calculadora.restar(5,3);
+        calculadora.restar(9,7);
+
+
+
+
+    }
+
+    public static void saludar(String data){
+        System.out.println("Hola "+data);
+        System.out.println("Buenos días");
+        System.out.println("Que tal estas");
+        System.out.println("Quieres un cafe");
+        System.out.println("Este método se va a repetir en el saludo");
+
+    }
+
+    public static void despedir(){
+        System.out.println("Hasta luego");
+        System.out.println("Que tengas buen día");
+    }
 
 }
